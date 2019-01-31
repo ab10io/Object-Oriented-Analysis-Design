73042ea84336427bc00e961b1e9712cc1fd95704
Sowmya Ramakrishnan <31675002+sowmya2910@users.noreply.github.com>
2019-01-30 22:10:14 -0700

    Uploading working code for HW1 Q4

--- Homework_1/MainClassMod.java
+++ Homework_1/MainClassMod.java
@@ -3,57 +3,55 @@ import java.util.Random;
 
 class Shape {
 	private int edges;
-    void displayShape() {}
-    public int getvalue(){return edges;}
-    
+	void displayShape(){}
+	public int getvalue(){return edges;}   
 }
 
 class Circle extends Shape {
 	private int edges=0;
 	public int getvalue() {return edges;}
-    void displayShape() { 	System.out.println("Circle");}
+	void displayShape() {System.out.println("Circle");}
 }
 
 class Square extends Shape {
-	 private int edges=4;
-	 public int getvalue() {return edges;}
-     void displayShape() { System.out.println("Square");}
+	private int edges=4;
+	public int getvalue() {return edges;}
+	void displayShape() {System.out.println("Square");}
  } 
 
 class Triangle extends Shape {
-	 private int edges=3;
-	 public int getvalue() {return edges;}
-     void displayShape() {System.out.println("Triangle");}
+	private int edges=3;
+	public int getvalue() {return edges;}
+	void displayShape() {System.out.println("Triangle");}
 }
  
-public class MainClassMod {  
-	
+public class MainClassMod { 
 	static Shape[]  sort(Shape[] k) {
 		for (int i=0; i<k.length; i++) {
-	    	for(int j=0; j<k.length; j++) {
-	    		if ((k[i]).getvalue() < k[j].getvalue()){
-	    			Shape temp= k[i];
-	    			k[i]= k[j];
-	    			k[j]=temp;
-	    } } }
+			for(int j=0; j<k.length; j++) {
+				if ((k[i]).getvalue() < k[j].getvalue()){
+					Shape temp= k[i];
+					k[i]= k[j];
+					k[j]=temp;
+				} 
+			} 
+		}
 		return k;
 	}
 	
-	
-    public static void main(String[] args) {
-    	
-    	Shape[] s = new Shape[9];
-        for (int i = 0; i < s.length; i++) {
-    		int n = (int)(Math.random() * 3 + 1);
-    		switch (n) {
-    		case 1: s[i]= new Circle();break;
-    		case 2: s[i]= new Square();break;
-    		case 3: s[i]= new Triangle();break;
-    		}
-        }
-        System.out.println("\nBefore Sorting");
+public static void main(String[] args) {
+	Shape[] s = new Shape[9];
+	for (int i = 0; i < s.length; i++) {
+		int n = (int)(Math.random() * 3 + 1);
+		switch (n) {
+			case 1: s[i]= new Circle();break;
+			case 2: s[i]= new Square();break;
+			case 3: s[i]= new Triangle();break;
+		}
+	}
+	System.out.println("\nBefore Sorting");
         System.out.println("#####################");
-        for (int i = 0; i < s.length; i++) {s[i].displayShape(); }
+	for (int i = 0; i < s.length; i++) {s[i].displayShape();}
         
         // SORTing
         sort(s);
@@ -61,6 +59,4 @@ public class MainClassMod {
         System.out.println("\nAfter Sorting");
         System.out.println("#####################");
         for (int i = 0; i < s.length; i++) {s[i].displayShape();}
-  }
-   
 }
