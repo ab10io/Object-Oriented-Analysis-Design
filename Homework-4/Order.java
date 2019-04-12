fdfc4e3b1929117094c61ca3c5dfa7e1614dc625
abhinav19792 <abhinav81728@gmail.com>
2019-04-12 10:23:44 -0600

    Create Order.java

---
+++ Homework-4/Order.java
@@ -0,0 +1,25 @@
+package abhinav;
+
+public class Order {
+	private int orderID;
+	private int dishID;
+	private int quantity;
+	
+	// Get the request from the web-form.
+	public void addDish() {
+		
+	}
+	
+	public void removeDish() {
+		
+	}
+	
+	public void getOrderStatus() {
+		
+	}
+	
+	public void cancelOrder() {
+		
+	}
+	
+}
