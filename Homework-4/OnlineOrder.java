c885fa455e183aa1a6be3d5721949d618823d9cc
abhinav19792 <abhinav81728@gmail.com>
2019-04-12 10:25:09 -0600

    Create OnlineOrder.java

---
+++ Homework-4/OnlineOrder.java
@@ -0,0 +1,16 @@
+package abhinav;
+
+public class OnlineOrder extends Order{
+	private String deliveryAddress;
+	
+	// Get delivery address from the web-form.
+	
+	public void assignDeliveryBoy () {
+		
+	}
+	
+	public void getOrderStatus() {
+		
+	}
+
+}
